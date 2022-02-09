d1263a60ec6b763e3f07a82010f3a963560a2694
yaochenghao <yaochenghao@bytedance.com>
2022-02-09 16:30:39 +0800

    init package

---
+++ src/background/index.ts
@@ -0,0 +1,7 @@
+(node:89798) ExperimentalWarning: The fs.promises API is experimental
+(node:89798) ExperimentalWarning: Readable[Symbol.asyncIterator] is an experimental feature. This feature could change at any time
+/**
+ * @desc background index
+ */
+
+console.log('test');
